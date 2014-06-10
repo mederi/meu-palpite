40c7cf63c6ca4cee9e2ff0cfe820163b50f38761
Jonata Weber <jonataweber@gmail.com>
2014-06-10 14:48:29 -0300

    Issue #1 Listar jogos

--- web/index.php
+++ web/index.php
@@ -1,12 +1,22 @@
 <?php
+
 require '../vendor/autoload.php';
 
-$app = new Silex\Application();
-$app['debug']=true;
-$app->register(new Silex\Provider\TwigServiceProvider(),array(
-  'twig.path'=> __DIR__.'/../app/views'));
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
 
-$app->get('/jogos/listar',function() use ($app){
-  return $app['twig']->render('jogos/listar.twig',array('name'=>'copa 2014'));
+$app = new Application();
+
+$app['debug'] = true;
+
+$app->register(new TwigServiceProvider(), array(
+  'twig.path'=> __DIR__.'/../app/views'
+));
+
+$app->get('/jogos/listar', function() use ($app) {
+  return $app['twig']->render('jogos/listar.twig', array(
+  	'name'=>'copa 2014'
+  ));
 });
+
 $app->run();
