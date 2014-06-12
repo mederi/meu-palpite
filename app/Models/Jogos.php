c15fcea2b07de743f10e3db5d3471352a07f1eed
Jonata Weber <jonataweber@gmail.com>
2014-06-12 15:47:38 -0300

    Issue #1 Listar jogos

--- app/Models/Jogos.php
+++ app/Models/Jogos.php
@@ -2,6 +2,8 @@
 
 namespace MeuPalpite\Models;
 
+use Respect\Relational\Sql;
+
 class Jogos
 {
 	protected $database;
@@ -13,13 +15,29 @@ class Jogos
 
 	public function getAll($times = array())
 	{
-		$jogos = $this->database->getMapper()->jogos->fetchAll();		
-		if (! empty($times))
-			array_map(function($jogo) use ($times) {				
+		$jogos = $this
+			->database
+			->getMapper()
+			->jogos
+			->fetchAll(Sql::orderBy('data_jogo')->asc());			
+
+		$dataAnterior = \DateTime::createFromFormat('Y-m-d', '2014-01-01');	
+
+		array_map(function($jogo) use ($times, &$dataAnterior) {				
+			if (! empty($times)) {			
 				$jogo->time_casa = $times[$jogo->time_id_casa - 1];
-				$jogo->time_visitante = $times[$jogo->time_id_visitante - 1];
-				return $jogo;
-			}, $jogos);
+				$jogo->time_visitante = $times[$jogo->time_id_visitante - 1];	
+			}			
+
+			$dataJogo = \DateTime::createFromFormat('Y-m-d H:i:s', $jogo->data_jogo);							
+			$jogo->pula = $dataJogo->format('d/m/Y') != $dataAnterior->format('d/m/Y');			
+			$dataAnterior = clone $dataJogo;
+
+			$jogo->data = $dataJogo->format('d/m/Y');
+			$jogo->hora = $dataJogo->format('H:i');
+			return $jogo;
+		}, $jogos);
+			
 
 		return $jogos;
 	}
